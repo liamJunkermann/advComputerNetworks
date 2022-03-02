fc31472987c985b8543c01078c1aa4564e251a23
Liam Junkermann <dev@ljsoft.xyz>
2022-03-02 11:46:02 +0000

    initial caching solution

---
+++ cache.go
@@ -0,0 +1,186 @@
+package main
+
+import (
+	"bufio"
+	"bytes"
+	"crypto/sha256"
+	"encoding/hex"
+	"fmt"
+	"hash"
+	"io"
+	"io/ioutil"
+	"os"
+	"sync"
+
+	"github.com/golang/glog"
+)
+
+type Cache struct {
+	folder      string
+	hash        hash.Hash
+	knownValues map[string][]byte
+	busyValues  map[string]*sync.Mutex
+	mutex       *sync.Mutex
+}
+
+func CreateCache(path string) (*Cache, error) {
+	fileInfos, err := ioutil.ReadDir(path)
+	if err != nil {
+		glog.Error("Cannot open cache folder ", path, ": ", err)
+		glog.Info("Create cache folder ", path)
+		os.Mkdir(path, os.ModePerm)
+	}
+
+	values := make(map[string][]byte, 0)
+	busy := make(map[string]*sync.Mutex, 0)
+
+	// Go through every file an save its name in the map. The content of the file
+	// is loaded when needed. This makes sure that we don't have to read
+	// the directory content each time the user wants data that's not yet loaded.
+	for _, info := range fileInfos {
+		if !info.IsDir() {
+			values[info.Name()] = nil
+		}
+	}
+
+	hash := sha256.New()
+
+	mutex := &sync.Mutex{}
+
+	cache := &Cache{
+		folder:      path,
+		hash:        hash,
+		knownValues: values,
+		busyValues:  busy,
+		mutex:       mutex,
+	}
+
+	return cache, nil
+}
+
+// Returns true if the resource is found, and false otherwise. If the
+// resource is busy, this method will hang until the resource is free. If
+// the resource is not found, a lock indicating that the resource is busy will
+// be returned. Once the resource has been put into cache the busy lock *must*
+// be unlocked to allow others to access the newly cached resource
+func (c *Cache) has(key string) (*sync.Mutex, bool) {
+	hashValue := calcHash(key)
+
+	c.mutex.Lock()
+	defer c.mutex.Unlock()
+
+	// If the resource is busy, wait for it to be free. This is the case if
+	// the resource is currently being cached as a result of another request.
+	// Also, release the lock on the cache to allow other readers while waiting
+	if lock, busy := c.busyValues[hashValue]; busy {
+		c.mutex.Unlock()
+		lock.Lock()
+		lock.Unlock()
+		c.mutex.Lock()
+	}
+
+	// If a resource is in the shared cache, it can't be reserved. One can simply
+	// access it directly from the cache
+	if _, found := c.knownValues[hashValue]; found {
+		return nil, true
+	}
+
+	// The resource is not in the cache, mark the resource as busy until it has
+	// been cached successfully. Unlocking lock is required!
+	lock := new(sync.Mutex)
+	lock.Lock()
+	c.busyValues[hashValue] = lock
+	return lock, false
+}
+
+func (c *Cache) get(key string) (*io.Reader, error) {
+	var response io.Reader
+	hashValue := calcHash(key)
+
+	// Try to get content. Error if not found.
+	c.mutex.Lock()
+	content, ok := c.knownValues[hashValue]
+	c.mutex.Unlock()
+	if !ok && len(content) > 0 {
+		glog.Info("Cache doesn't know key ", hashValue)
+		return nil, fmt.Errorf("Key '%s' is not known to cache", hashValue)
+	}
+
+	glog.Info("Cache has key", hashValue)
+
+	// Key is known, but not loaded into RAM
+	if content == nil {
+		glog.Info("Cache item ", hashValue, " known but is not stored in memory. Using file.")
+
+		file, err := os.Open(c.folder + hashValue)
+		if err != nil {
+			glog.Error("Error reading cached file ", hashValue, ": ", err)
+			return nil, err
+		}
+
+		response = file
+
+		glog.Info("Create reader from file ", hashValue)
+	} else { // Key is known and data is already loaded to RAM
+		response = bytes.NewReader(content)
+		glog.Info("Create reader from ", len(content), " byte large cache content")
+	}
+
+	return &response, nil
+}
+
+// release is an internal method which atomically caches an item and unmarks
+// the item as busy, if it was busy before. The busy lock *must* be unlocked
+// elsewhere!
+func (c *Cache) release(hashValue string, content []byte) {
+	c.mutex.Lock()
+	delete(c.busyValues, hashValue)
+	c.knownValues[hashValue] = content
+	c.mutex.Unlock()
+}
+
+func (c *Cache) put(key string, content *io.Reader, contentLength int64) error {
+	hashValue := calcHash(key)
+
+	// Small enough to put it into the in-memory cache
+	if contentLength <= config.MaxCacheItemSize*1024*1024 {
+		buffer := &bytes.Buffer{}
+		_, err := io.Copy(buffer, *content)
+		if err != nil {
+			return err
+		}
+
+		defer c.release(hashValue, buffer.Bytes())
+		glog.Info("Added ", hashValue, "into in-memory cache")
+
+		err = ioutil.WriteFile(c.folder+hashValue, buffer.Bytes(), 0644)
+		if err != nil {
+			return err
+		}
+		glog.Info("Wrote content of entry ", hashValue, " into file")
+	} else { // Too large for in-memory cache, just write to file
+		defer c.release(hashValue, nil)
+		glog.Info("Added nil-entry for ", hashValue, " into in-memory cache")
+
+		file, err := os.Create(c.folder + hashValue)
+		if err != nil {
+			return err
+		}
+
+		writer := bufio.NewWriter(file)
+		_, err = io.Copy(writer, *content)
+		if err != nil {
+			return err
+		}
+		glog.Info("Wrote content of entry ", hashValue, " into file")
+	}
+
+	glog.Info("Cache wrote content into ", hashValue)
+
+	return nil
+}
+
+func calcHash(data string) string {
+	sha := sha256.Sum256([]byte(data))
+	return hex.EncodeToString(sha[:])
+}
---
+++ main.go
@@ -0,0 +1,168 @@
+package main
+
+import (
+	"flag"
+	"io"
+	"log"
+	"net"
+	"net/http"
+	"net/http/httputil"
+	"strings"
+	"time"
+
+	"github.com/golang/glog"
+)
+
+var config *Config
+var cache *Cache
+
+func main() {
+	var addr = flag.String("addr", "127.0.0.1:8080", "The addr of the application.")
+	configPath := flag.String("config", "./tiny.json", "configuration .json file path")
+	flag.Parse()
+
+	loadConfig(*configPath)
+	glog.Info("Config Loaded")
+
+	prepare()
+
+	handler := &proxy{}
+
+	glog.Info("Starting proxy server on ", *addr)
+	if err := http.ListenAndServe(*addr, handler); err != nil {
+		glog.Fatal("ListenAndServe:", err)
+	}
+}
+
+func loadConfig(configPath string) {
+	var err error
+
+	config, err = LoadConfig(configPath)
+	if err != nil {
+		glog.Fatal("Could not read config: '%s'", err.Error())
+	}
+}
+
+func prepare() {
+	var err error
+	cache, err = CreateCache(config.CacheFolder)
+
+	if err != nil {
+		glog.Fatal("Could not init cache: '%s'", err.Error())
+	}
+}
+
+var hopHeaders = []string{
+	"Connection",
+	"Keep-Alive",
+	"Proxy-Authenticate",
+	"Proxy-Authorization",
+	"Te", // canonicalized version of "TE"
+	"Trailers",
+	"Transfer-Encoding",
+	"Upgrade",
+}
+
+func copyHTTPHeader(dst, src http.Header) {
+	for k, vv := range src {
+		for _, v := range vv {
+			dst.Add(k, v)
+		}
+	}
+}
+
+func delHopHeaders(header http.Header) {
+	for _, h := range hopHeaders {
+		header.Del(h)
+	}
+}
+
+func appendHostToXForwardHeader(header http.Header, host string) {
+	// If we aren't the first proxy retain prior
+	// X-Forwarded-For information as a comma+space
+	// separated list and fold multiple headers into one.
+	if prior, ok := header["X-Forwarded-For"]; ok {
+		host = strings.Join(prior, ", ") + ", " + host
+	}
+	header.Set("X-Forwarded-For", host)
+}
+
+type proxy struct{}
+
+func (p *proxy) ServeHTTP(wr http.ResponseWriter, req *http.Request) {
+	glog.Info(req.RemoteAddr, " ", req.Method, " ", req.URL, "Host: ", req.Host)
+	requestDump, _ := httputil.DumpRequest(req, true)
+	glog.Info(string(requestDump))
+
+	if req.Method != "CONNECT" { // if HTTP request
+		fullUrl := req.Host + req.URL.Path + "?" + req.URL.RawQuery
+		glog.Info("Requested: ", fullUrl)
+		client := &http.Client{}
+
+		if busy, ok := cache.has(fullUrl); !ok {
+			var startTime = time.Now()
+			defer busy.Unlock()
+			req.RequestURI = ""
+
+			delHopHeaders(req.Header)
+			if clientIP, _, err := net.SplitHostPort(req.RemoteAddr); err == nil {
+				appendHostToXForwardHeader(req.Header, clientIP)
+			}
+
+			resp, err := client.Do(req)
+			if err != nil {
+				http.Error(wr, "Server Error", http.StatusInternalServerError)
+				log.Fatal("ServeHTTP:", err)
+				return
+			}
+
+			var reader io.Reader
+			reader = resp.Body
+			var endTime = time.Now()
+			var totalTime = endTime.Sub(startTime)
+			glog.Info("Time Spent: ", totalTime)
+			err = cache.put(fullUrl, &reader, resp.ContentLength)
+			if err != nil {
+				http.Error(wr, "Server Error", http.StatusInternalServerError)
+				glog.Fatal("ServeHTTP:", err)
+				return
+			}
+			defer resp.Body.Close()
+		}
+
+		content, err := cache.get(fullUrl)
+		if err != nil {
+			http.Error(wr, "Server Error", http.StatusInternalServerError)
+			glog.Fatal("Serve from Cache", err)
+		} else {
+			contentWritten, err := io.Copy(wr, *content)
+			if err != nil {
+				glog.Fatal("Error writing response: ", err.Error())
+				return
+			}
+			glog.Info("Wrote ", contentWritten, " bytes")
+		}
+	} else {
+		glog.Info(strings.Index(req.Host, ":"))
+		if !strings.Contains(req.Host, ":") {
+			req.Host += ":80"
+		}
+		// Server connection
+		serverConn, err := net.Dial("tcp", req.Host)
+		if err != nil {
+			glog.Error(err)
+		}
+
+		// Access Client connection
+		hj, _ := wr.(http.Hijacker)
+		clientConn, _, hjErr := hj.Hijack()
+		if hjErr != nil {
+			glog.Error(hjErr)
+		}
+
+		clientConn.Write([]byte("HTTP/1.0 200 OK\r\n\r\n"))
+		go io.Copy(clientConn, serverConn)
+		_, srvErr := io.Copy(serverConn, clientConn)
+		glog.Info(srvErr)
+	}
+}
